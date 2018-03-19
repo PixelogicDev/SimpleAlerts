c35fa615ab65ed51f1090478809274bc6e11f6f3
Alec Dilanchian <alec.dilanchian@gmail.com>
2018-03-19 05:46:49 -0500

    Removing warning for component selector

--- SimpleAlerts/src/app/root/root.component.ts
+++ SimpleAlerts/src/app/root/root.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'sa-app',
   templateUrl: './root.component.html',
   styleUrls: ['./root.component.css']
