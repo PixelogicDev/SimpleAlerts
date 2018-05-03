e73c48cfaea012b0ca9719454671c2c2e6275ae6
Alec Dilanchian <alec.dilanchian@gmail.com>
2018-05-02 22:54:59 -0500

    Updating env file for production

--- SimpleAlerts/src/environments/environment.prod.ts
+++ SimpleAlerts/src/environments/environment.prod.ts
@@ -1,7 +1,11 @@
 export const environment = {
   production: true,
   envName: 'prod',
-  baseServerPath: '',
-  baseSimpleSocketPath: '',
-  streamLabsAuthUrl: ''
+  baseServerPath: 'https://simple-alerts.herokuapp.com/',
+  baseSimpleSocketPath: 'ws://simple-alerts.herokuapp.com',
+  streamLabsAuthUrl:
+    'https://www.streamlabs.com/api/v1.0/authorize' +
+    '?client_id=3cHN5exsWXQhEaKKvTkcuFQTA70Besv08T5aWMjw' +
+    '&redirect_uri=https://simplealerts.stream/dashboard' +
+    '&response_type=code&scope=donations.read+socket.token'
 };
