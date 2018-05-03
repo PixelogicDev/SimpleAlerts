eb290b3c4ef943b6f796557bbe62a2f4d8be5031
Alec Dilanchian <alec.dilanchian@gmail.com>
2018-05-03 05:30:27 -0500

    Adding server prop

--- SimpleAlerts-Server/websocket/ws.js
+++ SimpleAlerts-Server/websocket/ws.js
@@ -1,9 +1,10 @@
 // ws://<urlpath>:8000 || process port
 const WSServer = require('ws').Server;
+let server;
 if (process.env.NODE_ENV === 'dev') {
-  const server = require('http').createServer();
+  server = require('http').createServer();
 } else {
-  const server = require('https').createServer();
+  server = require('https').createServer();
 }
 const app = require('../app');
 const db = require('../database/db');
